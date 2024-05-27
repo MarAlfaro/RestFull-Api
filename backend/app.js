572dd278f91fb85afd52ddcf9741926b99afb0fa
Josue Coreas <josuepaiz487@gmail.com>
2024-05-27 09:16:07 -0600

    Adding FORM

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,40 +9,73 @@ app.use(express.json());
 let peliculas = [];
 
 //obtener todas las peliculas
-app.get('/peliculas', (req, res) => {
-    res.json(peliculas);
-})
+app.get("/peliculas", (req, res) => {
+  res.json(peliculas);
+});
 
 //crear una nueva pelicula
-app.post('/peliculas', (req, res) => {
-    const {titulo, protagonista, categoria, url} = req.body;
+app.post("/peliculas", (req, res) => {
+  const { titulo, protagonista, categoria, url } = req.body;
 
-    if (!titulo || !protagonista || !categoria || !url) {
-        return res.status(404).send("Todos los campos son obligatorios");
-    } else {
-        const nuevaPelicula = {
-            id: peliculas.length+1,
-            titulo,
-            protagonista,
-            categoria,
-            url
-        }
-    }
+  if (!titulo || !protagonista || !categoria || !url) {
+    return res.status(404).send("Todos los campos son obligatorios");
+  } else {
+    const nuevaPelicula = {
+      id: peliculas.length + 1,
+      titulo,
+      protagonista,
+      categoria,
+      url,
+    };
+  }
 
-    peliculas.push(nuevaPelicula);
-    res.status(201).send(nuevaPelicula);
-    
-})
+  peliculas.push(nuevaPelicula);
+  res.status(201).send(nuevaPelicula);
+});
 
 //ruta para obtener una pelicula en especifico
-app.get('/peliculas', (req, res) => {
-    const peliculaId = parseInt(req.params.id);
-    const pelicula = peliculas.find(p => p.id === peliculaId);
-    if (pelicula) {
-        res.json(pelicula);
-    } else {
-        res.status(404).send("No se encontro la pelicula");
-    }
+app.get("/peliculas/:id", (req, res) => {
+  const peliculaId = parseInt(req.params.id);
+  const pelicula = peliculas.find((p) => p.id === peliculaId);
+  if (pelicula) {
+    res.json(pelicula);
+  } else {
+    res.status(404).send("No se encontro la pelicula");
+  }
 });
 
 //ruta para actualizar una pelicula
+app.put("/peliculas/:id", (req, res) => {
+  const indexPelicula = peliculas.findIndex(
+    (pelicula) => pelicula.id === Number(req.params.id)
+  );
+
+  if (indexPelicula === -1) {
+    res.status(404);
+    return res.json({ error: "Pelicula no encontrada" });
+  }
+
+  peliculas[indexPelicula] = {
+    ...peliculas[indexPelicula],
+    ...req.body,
+  };
+
+  res.json(peliculas[indexPelicula]);
+});
+
+//Ruta para eliminar pelicula
+
+app.delete("/tasks/:id", (req, res) => {
+  const pelicula = peliculas.find(
+    (pelicula) => pelicula.id === Number(req.params.id)
+  );
+
+  if (!pelicula) {
+    res.status(404);
+    return res.json({ error: "Pelicula no encontrada" });
+  }
+
+  peliculas.splice(peliculas.indexOf(pelicula), 1);
+
+  res.json(pelicula);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <div className="container-input">
+          <label>Titulo</label>
+          <input />
+        </div>
+        <div className="container-input">
+          <label>Protagonista</label>
+          <input />
+        </div>
+        <div className="container-input">
+          <label>Categoria</label>
+          <input />
+        </div>
+        <div className="container-input">
+          <label>URL</label>
+          <input />
+        </div>
+      </form>
+      <button>Crear pelicula</button>
     </div>
   );
 }
